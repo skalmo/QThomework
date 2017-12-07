3d273bb9b7a7ecb454f26b8e31ef4313391adfcb
Eldar Mustafin <skalmo@rambler.ru>
2017-12-07 13:00:28 -0800

    Chart will be about currency from Bitcoin to Rub and Bitcoin to USD. Time slots 5 min

---
+++ HW/networkmanager.cpp
@@ -0,0 +1,86 @@
+#include "networkmanager.h"
+#include <QNetworkReply>
+#include <QNetworkRequest>
+
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+
+NetworkManager::NetworkManager(QObject *parent) : QObject(parent)
+{
+    manager = new QNetworkAccessManager();
+    connect(manager,SIGNAL(finished(QNetworkReply*)), this, SLOT(myReplyFinished(QNetworkReply*)));
+}
+
+void NetworkManager::loadWebPage(){
+    QNetworkRequest request;
+
+    QString apiKey = "H73X7AEFBZRJU1SU";
+    QString urlString = QString("https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_INTRADAY&symbol=BTC&market=RUB&apikey=%0").arg(apiKey);
+
+    QUrl url(urlString);
+
+    request.setUrl(url);
+
+    QNetworkReply *reply = manager->get(request);
+
+}
+
+//
+void NetworkManager::myReplyFinished(QNetworkReply*reply)
+{
+    QByteArray webData = reply->readAll();
+
+    QList<QPair<QString,QString>> graphValues;
+
+    //QString webDataString = QString(webData);
+
+    QJsonDocument document = QJsonDocument::fromJson(webData);
+
+    if(document.isArray() == true){
+        //retreive the array
+        QJsonArray rootArray = document.array();
+    }
+
+    else if(document.isObject() == true)
+    {
+
+        QJsonObject rootObject = document.object();
+
+        QJsonObject timeSeries = rootObject["Time Series (Daily)"].toObject();
+        QStringList keys = timeSeries.keys();
+        for (QString k : keys){
+            QJsonObject dayValues = timeSeries[k].toObject();
+            QString openValue =  dayValues["1. open"].toString();
+
+            QPair<QString,QString> dataItem;
+            dataItem.first = k;
+            dataItem.second = openValue;
+
+            graphValues.append(dataItem);
+        }
+
+/*
+        QStringList keys = rootObject.keys();
+        for (QString k : keys){
+
+
+            if(rootObject[k].isObject() == true){
+                QJsonObject object = rootObject[k].toObject();
+
+                for (QString childkey : object.keys()){
+                    qDebug() << childkey;
+                }
+            }
+        }
+*/
+    }
+
+    for (int i=0; i<graphValues.size(); i++){
+        QPair<QString,QString> data = graphValues[i];
+        qDebug()<<data.first <<" - "<<data.second;
+    }
+
+
+    //qDebug()<<webDataString;
+}
